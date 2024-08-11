d1292ad77bf2d99de7ab4aed223bea3411477fb1
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com.>
2024-08-11 14:19:01 +0500

    post method

--- 3-express/app.js
+++ 3-express/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
+app.post("/messages", (req, res) =>
+  res.send("This is where you can see any messages.")
+);
 
 const PORT = 3000;
 app.listen(PORT, () =>
